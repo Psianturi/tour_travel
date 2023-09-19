22c2330388a349443aab5df87f37402e12277799
Psianturi <posma2018@gmail.com>
2023-09-19 15:25:41 +0700

    Update codes

--- lib/api/tour_travel_api.dart
+++ lib/api/tour_travel_api.dart
@@ -1,6 +1,7 @@
 
 class TourTravelApi {
-static String baseUrl = 'http://192.168.22.23:8080/api/v1';
+static String baseUrl = 'https://api-qtracking.jtisrv.com/api/v2';
+    // 'http://192.168.22.23:8080/api/v1';
 static String imageUrl = 'https://storage.googleapis.com/';
 
 static String getList = '/tourntravel/get_list';
